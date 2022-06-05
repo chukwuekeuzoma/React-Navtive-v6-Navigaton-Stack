8693b9b9aa5ec4705d5baa9321f3e63eb8abc440
mac <chukwuekeuzoma@gmail.com>
2022-06-05 21:05:50 +0100

    updated the component

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
+import ReviewDetails from "./Screens/ReviewDetails";
+// import Navigator from "./Routes/drawer";
+import * as SplashScreen from "expo-splash-screen";
+import { GloblalStyles } from "./Styles/GloblalStyles";
+import Home from "./Screens/Home";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  useCallback(async () => {
+    if (!fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Text>Martins Loading...</Text>
+      </View>
+    );
+  }
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "Home",
+            headerStyle: {
+              backgroundColor: "yellow",
+            },
+          }}
+        />
+        <Stack.Screen name="Details" component={ReviewDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 20,
   },
 });
