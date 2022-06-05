fd6b44d26889424131e5135a256aacbbef92cc33
mac <chukwuekeuzoma@gmail.com>
2022-06-05 21:07:02 +0100

    update the component

---
+++ Styles/GloblalStyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const GloblalStyles = StyleSheet.create({
+    titleText:{
+        fontFamily:"Inter_900Black",
+        marginTop:50,
+        flex: 1
+    }
+})
+
