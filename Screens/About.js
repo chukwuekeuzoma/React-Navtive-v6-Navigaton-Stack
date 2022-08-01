e7fe5c97038c6917c3d7068a76da0dd48f9a3007
mac <chukwuekeuzoma@gmail.com>
2022-08-01 11:32:52 +0100

    updated the component

--- Screens/About.js
+++ Screens/About.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import {View, Text, StyleSheet} from "react-native"
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 function About(props) {
-    return (
-        <View className={styles.container}>
-            <Text>About Page</Text> 
-        </View>
-    );
+  return (
+    <View className={styles.container}>
+      <Text>About Page</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        padding:24,
-        flex: 1
-    }
-})
+  container: {
+    padding: 24,
+    flex: 1,
+  },
+});
 
 export default About;
